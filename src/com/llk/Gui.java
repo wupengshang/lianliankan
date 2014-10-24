34e939aa6ffe2be2abce1d06f0dbba3838fd971b
wupengshang <lijiashengsdu@gmail.com>
2014-10-24 17:09:14 +0800

    修改了所有都能消除的问题
    引入了部分特殊情况不能消除的问题，fuck！！

--- src/com/llk/Gui.java
+++ src/com/llk/Gui.java
@@ -20,7 +20,11 @@ class MainGui extends JFrame{
 	public static Picture [][] temp = new Picture [10][8]; 
 	public static ImageIcon[] images = new ImageIcon[10];//10种图片类型
 	public static LinkedList<Picture> link = new LinkedList<Picture>();
+	public static LinkedList<Location> saved = new LinkedList<Location>();
+	public static Location[] check_cross = new Location[3];
 	public static int realtype = 0;
+	public static boolean status = false;
+	public final static int CROSSNUM = 2;
 	Random ran2 = new Random();
 	
 	
@@ -79,6 +83,7 @@ class MainGui extends JFrame{
 					temp[second_loc.x][second_loc.y].setVisible(false);
 					temp[first_loc.x][first_loc.y].type = -1;//change the type to -1/empty
 					temp[second_loc.x][second_loc.y].type = -1;
+					status = false;
 				}				
 			}
 			else{
@@ -96,7 +101,7 @@ class MainGui extends JFrame{
 		Picture p2 = new Picture();
 		Picture exc = new Picture();
 		int x1,x2,y1,y2;
-		boolean status = false;
+
 		x1 = l1.x;
 		x2 = l2.x;
 		y1 = l1.y;
@@ -111,16 +116,16 @@ class MainGui extends JFrame{
 		while((exc = link.poll()) != null){
 			exc.colour = 2;
 			exc.state = true;
-	
-			if(exc.lp.x == x2 && exc.lp.y == y2){
-				//System.out.println("success");
-				status = true;
-				//printroad(nodes[4][4]);
+			if(status == false){
+				if(exc.lp.x == x2 && exc.lp.y == y2){
+					continue;
+				}
+				else{
+					search(exc);	
+				}
+			}
+			else
 				break;
-			}
-			else{
-				search(exc);	
-			}
 		}	
 				
 		return status;
@@ -133,29 +138,98 @@ class MainGui extends JFrame{
 			temp[x1+1][y1].x0 = x1;
 			temp[x1+1][y1].y0 = y1;
 			link.offer(temp[x1+1][y1]);	
-			System.out.println("1");
+			if((x1+1) == second_loc.x && y1 == second_loc.y){
+				temp[x1+1][y1].colour = 0;
+				saveroad(temp[x1+1][y1]);
+				if(checkcross()){
+					status = true;
+				}
+				else{
+					status = false;
+					refresh();
+				}
+			}			
 		}//下边一个
 		if((x1 > 0)&&(temp[x1-1][y1].state == false)&&(temp[x1-1][y1].type == realtype || temp[x1-1][y1].type == -1)){
 			temp[x1-1][y1].colour = 1;//灰色入队列
 			temp[x1-1][y1].x0 = x1;
 			temp[x1-1][y1].y0 = y1;
 			link.offer(temp[x1-1][y1]);	
-			System.out.println("2");
+			if((x1-1) == second_loc.x && y1 == second_loc.y){
+				temp[x1-1][y1].colour = 0;
+				saveroad(temp[x1-1][y1]);
+				if(checkcross()){
+					status = true;
+				}
+				else{
+					status = false;
+					refresh();
+				}
+			}
 		}//上边一个
 		if((y1 > 0)&&(temp[x1][y1-1].state == false)&&(temp[x1][y1-1].type == realtype || temp[x1][y1-1].type == -1)){
 			temp[x1][y1-1].colour = 1;//灰色入队列
 			temp[x1][y1-1].x0 = x1;
 			temp[x1][y1-1].y0 = y1;
 			link.offer(temp[x1][y1-1]);
-			System.out.println("3");
+			if((x1) == second_loc.x && (y1-1) == second_loc.y){
+				temp[x1][y1-1].colour = 0;
+				saveroad(temp[x1][y1-1]);
+				if(checkcross()){
+					status = true;
+				}
+				else{
+					status = false;
+					refresh();
+				}
+			}
 		}//左边一个
 		if((y1 < temp[0].length - 1)&&(temp[x1][y1+1].state == false)&&(temp[x1][y1+1].type == realtype || temp[x1][y1+1].type == -1)){
 			temp[x1][y1+1].colour = 1;//灰色入队列
 			temp[x1][y1+1].x0 = x1;
 			temp[x1][y1+1].y0 = y1;
 			link.offer(temp[x1][y1+1]);
-			System.out.println("4");
-		}//左边一个
+			if((x1) == second_loc.x && (y1+1) == second_loc.y){
+				temp[x1][y1+1].colour = 0;
+				saveroad(temp[x1][y1+1]);
+				if(checkcross()){
+					status = true;
+				}
+				else{
+					status = false;
+					refresh();
+				}
+			}
+		}//右边一个
+	}
+	public static void saveroad(Picture end){
+		if(!((end.lp.x == first_loc.x) && (end.lp.y == first_loc.y))){
+			saveroad(temp[end.x0][end.y0]);
+		}
+		saved.offer(end.lp);
+		System.out.println("(" + end.lp.x + "," + end.lp.y + ")");
+	}
+	public static boolean checkcross(){
+		Location temp1 = new Location();
+		int cross = 0;
+		int count = 0;
+		while((temp1 = saved.poll()) != null){
+			check_cross[count] = temp1;
+			if(count == 2){
+				if(Math.abs(check_cross[0].x - check_cross[2].x) == 1 && Math.abs(check_cross[0].y - check_cross[2].y) == 1){
+					cross++;
+				}
+				check_cross[0] = check_cross[1];
+				check_cross[1] = check_cross[2];
+				count = 1;
+			}
+			count++;
+		}
+		System.out.println("cross=" + cross);
+		if(cross <= CROSSNUM)
+			return true;
+		else
+			return false;
 	}
 	public static void refresh(){
 		for(int i=0;i< temp.length;i++){
@@ -165,6 +239,7 @@ class MainGui extends JFrame{
 			}
 		}
 		while(link.poll() != null);
+		while(saved.poll() != null);
 		System.out.println("5");
 	}
 	public static void imageinit(){
